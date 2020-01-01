bb2522b0b2df82b5bf5ed77cd7a494a2ac45bab0
duanmengting <654623305@qq.com>
2020-01-01 19:20:45 +0800

    B 修改 Test1

--- src/Test1.java
+++ src/Test1.java
@@ -3,7 +3,7 @@ public class Test1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World 11");
+        System.out.println("Hello World 33");
         System.out.println("test1");
     }
 }
